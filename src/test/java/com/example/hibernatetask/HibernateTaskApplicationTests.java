fda85346c1ca7ac113f23a8a65b6fd545f7a5ebb
Raman Rashchupkin <rrashchupkin@piesoft.us>
2022-05-23 15:24:47 +0300

    Initial commit

---
+++ src/test/java/com/example/hibernatetask/HibernateTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernatetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
